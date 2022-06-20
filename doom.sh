2320e1920629cf8ea1c0542a7c49aebd6f4565f8
Fabian <77250276+ParadoxialN3@users.noreply.github.com>
2022-06-19 23:09:06 -0400

    cleaning the script up

--- doom.sh
+++ doom.sh
@@ -6,12 +6,9 @@ sudo dnf install emacs ripgrep -y
 I will now install DOOM Emacs.
 sleep 2
 git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+cp ~/dotfiles/.doom.d/config.el ~/.doom.d
+cp ~/dotfiles.doom.d/init.el ~/.doom.d
 echo Say yes to everything it prompts you for.
 sleep 3
 ~/.emacs.d/bin/doom install
-echo Now I will copy my configurations.
-sleep 2
-cp ~/dotfiles/.doom.d/config.el ~/.doom.d
-cp ~/dotfiles/.doom.d/init.el ~/.doom.d
-echo If you already have my .zshrc you already have the paths setup so you do not need to do ~/.emacs.d/bin/doom <insert argument here>. Just restart zsh and do a DOOM Sync. After that, enjoy.
-sleep 3
+echo If you use my zshrc your DOOM Emacs paths are already set up. Run `doom sync` or `M-x doom/reload` if you do not want to quit emacs every time you edit a file in ~/.doom.d. Enjoy DOOM Emacs.
